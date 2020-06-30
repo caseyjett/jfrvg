d18c672b52cdf24fbb6d78959702734cf2be15ee
Casey Jett <crjett01@gmail.com>
2020-06-30 11:22:23 -0400

    AddEventListener for page resizing

--- scripts.js
+++ scripts.js
@@ -2,19 +2,33 @@
   // Look for .hamburger
   const hamburger = document.querySelector(".hamburger");
   const header = document.querySelector("header");
+  const menu = document.getElementsByClassName("nav-pages");
   // On click
   hamburger.addEventListener("click", function() {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
-    let menu = document.getElementsByClassName("nav-pages");
+    
+    let windowWidth = window.innerWidth; 
     for (let i = 0; i < menu.length; i++) {
-    if (hamburger.classList.contains("is-active")) {
+      
+      if (hamburger.classList.contains("is-active") && windowWidth < 992) {
+        menu[i].style.display = "block"; 
+      } else {
+        menu[i].style.display = "none"; 
+      }
+  }
+  });
+
+  window.addEventListener('resize', function (e) {
+    var currentWidth = window.innerWidth;
+    for (let i = 0; i < menu.length; i++) {
+    if (currentWidth >= 992) { 
       menu[i].style.display = "block"; 
     } else {
       menu[i].style.display = "none"; 
-    }}
-  });
+    }
+  }});
 
 
-// Why do I have to clik the hambuger twice before the list appears?
+
