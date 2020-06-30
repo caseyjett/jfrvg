c2e0308b3eb2f6ff1eec6afbc52fa680785cfef3
Casey Jett <crjett01@gmail.com>
2020-06-30 00:19:03 -0400

    Fixed hamburger menu so that all pages show

--- scripts.js
+++ scripts.js
@@ -1,18 +1,20 @@
 
   // Look for .hamburger
   const hamburger = document.querySelector(".hamburger");
+  const header = document.querySelector("header");
   // On click
   hamburger.addEventListener("click", function() {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
-    let menu = document.querySelector(".nav-pages");
-    if (hamburger.classList.contains("is-active") && menu.style.display === "none") {
-      menu.style.display = "block"; 
+    let menu = document.getElementsByClassName("nav-pages");
+    for (let i = 0; i < menu.length; i++) {
+    if (hamburger.classList.contains("is-active") && menu[i].style.display === "none") {
+      menu[i].style.display = "block"; 
     } else {
-      menu.style.display = "none"; 
-    }
+      menu[i].style.display = "none"; 
+    }}
   });
 
 
-
+// Why do I have to clik the hambuger twice before the list appears?
